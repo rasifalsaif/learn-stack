325181b5177c734aec0710038d066a45cfe52fff
rasifalsaif <rasifalsaif@gmail.com>
2026-04-26 17:44:32 +0600

    add JSDoc documentation to cn utility function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,13 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+/**
+ * Merges class names using clsx and tailwind-merge.
+ * This utility allows for conditional classes and resolves Tailwind CSS conflicts.
+ * 
+ * @param inputs - A list of class values to be merged.
+ * @returns A single string of merged class names.
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
